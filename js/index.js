78592376376262b165e54d898467c7494b750ad0
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-06-30 17:35:42 -0300

    exemplo de async / await

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const urlBase = "https://viacep.com.br/ws/01001000/json/"
 
+/*
 fetch(urlBase)
   .then((response) => {
     response.json().then((dados) => console.log(dados) )
@@ -8,5 +9,13 @@ fetch(urlBase)
   .catch((error) => {
     console.log(error);
   });
+    */
+  // async / await 
 
-  
+  async function obterDados() {
+    const resposta = await fetch(urlBase)
+    const  dados = await resposta.json()
+    console.log(dados)
+  }
+
+  obterDados()
