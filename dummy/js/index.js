ec11074b37775f19d188545a21f9fc4625393d46
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-01 15:50:35 -0300

    feat(getUseres): função finalizada

--- dummy/js/index.js
+++ dummy/js/index.js
@@ -1,11 +1,17 @@
 async function getUsers() {
-    
+    // passando Header dentro do fetch, passando como objeto 
+  const resposta = await fetch("https://dummyapi.io/data/v1/user?created=1", {
+    headers: {
+        "app-id": "25478965214533654122"
+    }
+});
+
+  const users = await resposta.json();
+  console.log(users.data); // array com os usuarios 
 }
 
-async function getUser() {
-    
-}
+getUsers()
 
-async function createUser() {
-    
-}
+async function getUser() {}
+
+async function createUser() {}
