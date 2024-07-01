661472a05aefca9a329a63ac9be88debab7375d2
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-07-01 16:03:54 -0300

    feat(createUser): função finalizada

--- dummy/js/index.js
+++ dummy/js/index.js
@@ -14,4 +14,25 @@ getUsers()
 
 async function getUser() {}
 
-async function createUser() {}
+async function createUser() {
+    const userDate ={
+        firstName: "Jefferson",
+        lastName: "Mendes",
+        email: "teste@gmail.com"
+    }
+
+    //fazendo uma requisição do tipo post 
+    try{
+        await fetch("https://dummyapi.io/data/v1/user/create", {
+            method: "POST",
+            headers: {
+                "app-id": "25478965214533654122",
+                "Content-type": "aplication/json" // esse foi criado no insomnia quando foi feito o teste, entao preciso oassar aqui 
+            },
+            //no body nao posso passar um objeto do js , tenho que converter 
+            body: JSON.stringify(userDate)
+        })
+    } catch (erro) {
+        console.log("deu erro ao criar o usuario")
+    }
+}
